e0f3b8814020e910af241493594a3550331a3d55
Jock <jockhuang@gmail.com>
2012-01-13 15:21:02 +0800

    索引循环次数

---
+++ src/main/java/biz/qianyan/search/util/Config.java
@@ -0,0 +1,14 @@
+/**
+ * Specl.com Inc.
+ * Copyright (c) 2010-2012 All Rights Reserved.
+ */
+package biz.qianyan.search.util;
+
+/**
+ * 
+ * @author Jock
+ */
+public final class Config {
+    public final static int INDEX=1000;
+
+}
